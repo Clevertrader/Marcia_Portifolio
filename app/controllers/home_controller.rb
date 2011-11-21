129c70d11685a38f6eaf97a122500f70c4fa68db
Felipe Sewaybricker Lombardi <felipesewaybricker@gmail.com>
2011-11-20 22:53:17 -0200

    removido valores no controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,11 @@ class HomeController < ApplicationController
 	layout 'home'
 
 	def index
-		@current = true
 	end
 
 	def portifolio
-		@current = true
 	end
 
 	def contato
-		@current = true
 	end
 end
